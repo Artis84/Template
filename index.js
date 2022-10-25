2e5acc30dbf31b8e767e558502760f61669243c6
Artis84 <lopsta10@gmail.com>
2022-10-25 09:17:08 +0200

    Remove main field and edit react component

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
+import { Component } from "react";
 import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById("root"));
-function tick() {
-    const element = (
-        <div>
-            <h1 class="text-blue-600">Hello, world!</h1>
-            <h2 class="text-gray-400">{new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
-    root.render(element);
+// function Hour(props) {
+// return (
+//     ...
+// )
+class Hour extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+        };
+    }
+
+    componentDidMount() {
+        console.log("Component Mounted !🤩");
+        this.timerID = setInterval(
+            () =>
+                this.setState({
+                    date: new Date(),
+                }),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        console.log("removed !💣");
+        clearInterval(this.timerID);
+    }
+
+    // tick() {
+    //     this.setState({
+    //         date: new Date(),
+    //     });
+    // }
+    render() {
+        return (
+            <div>
+                <h1 className="text-blue-600">{this.props.title}</h1>
+                {/* <h2 className="text-gray-400">{new Date().toLocaleTimeString()}</h2> */}
+                <h2 className="text-gray-400">{this.state.date.toLocaleTimeString("fr-FR")}</h2>
+            </div>
+        );
+    }
 }
 
-setInterval(tick, 1000);
+const root = createRoot(document.getElementById("root"));
+
+// Whitout state property
+// function tick() {
+// Add a property as an atribute to the component
+// const element = <Hour title="Bienvenue" />;
+root.render(<Hour title="Bienvenue" />);
+// }
+console.log("test");
+
+// setInterval(tick, 1000);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,13 @@
 module.exports = {
     // prettier-ignore
     content: [
-        "./public/**/*.{html,js,vue}", 
+        "./public/**/*.{html,js,vue,jsx}", 
         "./index.js"
     ],
     theme: {
+        fontFamily: {
+            Overpass: ["Overpass", "sans-serif"],
+        },
         extend: {},
     },
     plugins: [],
